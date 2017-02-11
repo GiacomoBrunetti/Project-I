49cedfdf12ad7440871b3c733203b934044cbd57
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2017-02-11 15:46:56 +0000

    added the net

--- js/app.js
+++ js/app.js
@@ -9,48 +9,60 @@ $(() => {
   const $box35 = $('#35');
   const $play = $('button');
   const $lives = $('.lives');
+  const $score = $('.score');
+  let score = 0;
   const livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
+  let x = 1;
+
+  $play.focus();
 
   $lives.html(livesNumber);
-
-
-
-
+//move net function
+  $('body').keydown(function(e) {
+    // if (divNumber>=29) {
+      //keypress rigth moves right
+    if (e.keyCode === 39) {
+      console.log('pressed right');
+      $('.box'+ (30+x)).addClass('net');
+      $('.box'+ (30+x-1)).removeClass('net');
+      x= x+1;
+      console.log(x);
+    //keypress left moves left
+    } else if(e.keyCode === 37) {
+      console.log('pressed left');
+      $('.box'+ (30+x-1)).addClass('net');
+      $('.box'+ (30+x)).removeClass('net');
+      x= x-1;
+      console.log(x);
+    }
+    //}
+  });
 
 //select random div to generate animal
   function randomDiv() {
     divNumber = Math.floor(Math.random()*6);
   }
 
-
-
-
-
-
 //generate the animal
   function generateAnimal() {
     randomDiv();
     $('.box'+divNumber).addClass('cat');
-    console.log(divNumber);
-
   }
+  console.log(divNumber);
+
   $play.on('click', ()=>{
     generateAnimal();
     fallOne();
-    keepGoing();
+    // keepGoing();
 
   });
 // create loop
-  function keepGoing() {
-    while(livesNumber>0){
-      generateAnimal();
-      fallOne();
-    }
-  }
-
-
-
-
+  // function keepGoing() {
+  //   while (livesNumber.length>0) {
+  //     generateAnimal();
+  //     fallOne();
+  //   }
+  // }
 
   // Make the animal fall
   function fallOne() {
@@ -76,26 +88,32 @@ $(() => {
       } else
       clearInterval(timerId);
 
+      // function to make you lose lifes
       function lifeLoss() {
 
         const $bottom = [$box30.attr('class').toString(), $box31.attr('class').toString(), $box32.attr('class').toString(), $box33.attr('class').toString(), $box34.attr('class').toString(), $box35.attr('class').toString()];
 
         console.log($bottom);
+        if ($bottom.includes('box30 net cat')||$bottom.includes('box31 net cat')||$bottom.includes('box32 net cat')||$bottom.includes('box33 net cat')||$bottom.includes('box34 net cat')||$bottom.includes('box35 net cat')) {
 
-        if ($bottom.includes('box30 cat')||$bottom.includes('box31 cat')||$bottom.includes('box32 cat')||$bottom.includes('box33 cat')||$bottom.includes('box34 cat')||$bottom.includes('box35 cat')) {
+          console.log('you caught a cat!');
+          score = score+1;
+          $score.text(score);
+
+        } else if ($bottom.includes('box30 cat')||$bottom.includes('box31 cat')||$bottom.includes('box32 cat')||$bottom.includes('box33 cat')||$bottom.includes('box34 cat')||$bottom.includes('box35 cat')) {
+
           alert('you lost a life');
+
           livesNumber.pop();
           $lives.html(livesNumber);
         }
       }
-
       lifeLoss();
-
-
-
-      // function to make you lose lifes
-
     }, falltime);
+    function clear() {
+      
+    }
+    clear();
   }
 
 
@@ -105,6 +123,6 @@ $(() => {
 
 
 
-
+  // keepGoing();
 
 });
