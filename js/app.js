550c1ef998ae79fe081185f6e9ef411fbc0a3617
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2017-02-13 18:21:16 +0000

    added styling and added the dogs falling, also added the hand to catch the animals- yes first commit of the day, totally forgot

--- js/app.js
+++ js/app.js
@@ -1,128 +1,135 @@
 $(() => {
   console.log('all working boss');
   let divNumber = null;
-  const $box30 = $('#30');
-  const $box31 = $('#31');
-  const $box32 = $('#32');
-  const $box33 = $('#33');
-  const $box34 = $('#34');
-  const $box35 = $('#35');
-  const $play = $('button');
+  let className = null;
+  const $caught = $('#caught').get(0);
+  const $byeLife = $('#lifeLost').get(0);
+  const $divs = $('div');
+  const $play = $('#play');
+  const $stopTrack = $('#stop');
+  const $RedPlanet = $('#soundtrack').get(0);
   const $lives = $('.lives');
   const $score = $('.score');
+  const $section = $('section');
   let score = 0;
-  const livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
-  let x = 1;
-
+  const livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
+  let x = 0;
+  console.log(livesNumber.length);
+  $section.hide();
+  $divs.hide();
+  $score.hide();
   $play.focus();
+  $play.on('click', ()=> {
+    $section.show();
+    $divs.show();
+    $score.show();
+    $play.hide();
+    $stopTrack.hide();
+  });
+  $stopTrack.on('click', () => {
+    $RedPlanet.pause();
+  });
 
   $lives.html(livesNumber);
 //move net function
   $('body').keydown(function(e) {
-    // if (divNumber>=29) {
+
       //keypress rigth moves right
     if (e.keyCode === 39) {
-      console.log('pressed right');
-      $('.box'+ (30+x)).addClass('net');
-      $('.box'+ (30+x-1)).removeClass('net');
-      x= x+1;
-      console.log(x);
+      if (x < 5) {
+        console.log('pressed right');
+        x= x+1;
+        $('.box'+ (42+x)).addClass('net');
+        $('.box'+ (42+x-1)).removeClass('net');
+        console.log(x);
+      }
     //keypress left moves left
     } else if(e.keyCode === 37) {
-      console.log('pressed left');
-      $('.box'+ (30+x-1)).addClass('net');
-      $('.box'+ (30+x)).removeClass('net');
-      x= x-1;
-      console.log(x);
+      if(x >0){
+        console.log('pressed left');
+        x= x-1;
+        $('.box'+ (42+x)).addClass('net');
+        $('.box'+ (42+x+1)).removeClass('net');
+        console.log(x);
+        // console.log(x);
+      }
     }
-    //}
-  });
 
-//select random div to generate animal
-  function randomDiv() {
-    divNumber = Math.floor(Math.random()*6);
-  }
+  });
 
 //generate the animal
   function generateAnimal() {
-    randomDiv();
-    $('.box'+divNumber).addClass('cat');
+    divNumber = Math.floor(Math.random()*6);
+    className = Math.random() > 0.5 ? 'cat' : 'dog';
+    $(`.box${divNumber}`).addClass(className);
   }
   console.log(divNumber);
 
-  $play.on('click', ()=>{
+  $play.on('click', () => {
+
     generateAnimal();
     fallOne();
-    // keepGoing();
 
   });
-// create loop
-  // function keepGoing() {
-  //   while (livesNumber.length>0) {
-  //     generateAnimal();
-  //     fallOne();
-  //   }
-  // }
 
   // Make the animal fall
+  let falltime = 200;
+
   function fallOne() {
-    let falltime = 1000;
-    fallTime();
-    function fallTime() {
-      if (fallTime>=300){
-        var timerId2 = setInterval(() => {
-          falltime = falltime - 100;
-        }, 30000);
-      } clearInterval(timerId2);
+    // if (livesNumber>=0){
+    if (falltime>= 100){
+      falltime--;
     }
+    setTimeout(() => {
 
-    var timerId = setInterval(() => {
+      if (divNumber <= 41) {
+        if (className==='cat'){
+          $(`.box${divNumber}`).removeClass('cat');
 
-      if (divNumber<=35){
-        $('.box'+(divNumber)).removeClass('cat');
-        divNumber += 6;
+          divNumber += 6;
 
-        $('.box'+(divNumber)).addClass('cat');
-        console.log('fallOne', divNumber);
-
-      } else
-      clearInterval(timerId);
-
-      // function to make you lose lifes
-      function lifeLoss() {
-
-        const $bottom = [$box30.attr('class').toString(), $box31.attr('class').toString(), $box32.attr('class').toString(), $box33.attr('class').toString(), $box34.attr('class').toString(), $box35.attr('class').toString()];
-
-        console.log($bottom);
-        if ($bottom.includes('box30 net cat')||$bottom.includes('box31 net cat')||$bottom.includes('box32 net cat')||$bottom.includes('box33 net cat')||$bottom.includes('box34 net cat')||$bottom.includes('box35 net cat')) {
-
-          console.log('you caught a cat!');
-          score = score+1;
-          $score.text(score);
-
-        } else if ($bottom.includes('box30 cat')||$bottom.includes('box31 cat')||$bottom.includes('box32 cat')||$bottom.includes('box33 cat')||$bottom.includes('box34 cat')||$bottom.includes('box35 cat')) {
-
-          alert('you lost a life');
-
-          livesNumber.pop();
-          $lives.html(livesNumber);
+          $(`.box${divNumber}`).addClass('cat');
+          // console.log('fallOne', divNumber);
+          fallOne();
+        } else {
+          $(`.box${divNumber}`).removeClass('dog');
+          divNumber += 6;
+          $(`.box${divNumber}`).addClass('dog');
+          // console.log('fallOne', divNumber);
+          fallOne();
         }
+      } else {
+        checkCatch();
+        generateAnimal();
+        fallOne();
       }
-      lifeLoss();
     }, falltime);
-    function clear() {
-      
-    }
-    clear();
+      // console.log('end'+ divNumber);
   }
 
+  function checkCatch() {
+    switch($(`.box${divNumber}`).hasClass('net')) {
+      case true:
+        console.log('you caught an animal'+`.box${divNumber}`);
+        score = score+1;
+        $caught.play();
+        $score.text(score);
+        break;
+      case false:
+        console.log('you lost a life');
+        $byeLife.play();
+        livesNumber.pop();
+        $lives.html(livesNumber);
 
+    } ($(`.box${divNumber}`).removeClass('cat dog'));
+  }
 
-
-
-
-
-
-  // keepGoing();
+  // function removeLife() {
+  //   if (checkCatch === false){
+  //     livesNumber.pop();
+  //     $lives.html(livesNumber);
+  //   }
+  // }
+  //
 
 });
