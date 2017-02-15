2cfee1c867d6849ca39456b407d3f3c10064c570
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2017-02-15 16:45:18 +0000

    final style of the projet(for desktop browsers)

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(() => {
   let divNumber = null;
   let className = null;
   const $gameOver = $('.gameOver');
+  const $article = $('article');
   const $caught = $('#caught').get(0);
   const $byeLife = $('#lifeLost').get(0);
   const $divs = $('div');
@@ -12,41 +13,46 @@ $(() => {
   const $lives = $('.lives');
   const $score = $('.score');
   const $section = $('section');
+  const $playAgain = $('#playAgain');
   const $left = $('#left');
   const $right = $('#right');
-  const $h1 = $('h1');
-  const $h2 = $('h2');
-  const $playBtn = $('#play');
-  const $button = $('button');
   let score = 0;
+  let falltime = 250;
   const livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
   let x = 0;
   console.log(livesNumber.length);
 
-  //detect if mobile, if so show buttons
-  detectmob();
-  if (document.body.className==='touch') {
-    $left.show();
-    $right.show();
-    screen.lockOrientation('landscape');
-
-  } else {
-    $left.hide();
-    $right.hide();
-  }
+  //detect mobile, if so show buttons
+  // detectmob();
+  // if (document.body.className==='touch') {
+  //   $left.show();
+  //   $right.show();
+  //   screen.orientation.lock('landscape');
+  //   $gameOver.hide();
+  //   $section.hide();
+  //   $divs.hide();
+  //   $score.hide();
+  //   $article.hide();
+  //
+  // } else {
+  //   $left.hide();
+  //   $right.hide();
+  // }
 
   $right.on('click', () => {
     if (x < 5) {
-      console.log('pressed right');
       x= x+1;
       $('.box'+ (42+x)).addClass('net');
       $('.box'+ (42+x-1)).removeClass('net');
-      console.log(x);
     }
   });
 
   $left.on('click', ()=> {
-
+    if(x >0){
+      x= x-1;
+      $('.box'+ (42+x)).addClass('net');
+      $('.box'+ (42+x+1)).removeClass('net');
+    }
   });
 
   $lives.html(livesNumber);
@@ -54,7 +60,11 @@ $(() => {
   $section.hide();
   $divs.hide();
   $score.hide();
+  $playAgain.hide();
   $play.focus();
+  $article.show();
+  $right.hide();
+  $left.hide();
 
   $play.on('click', ()=> {
     $('html, body').animate({ scrollTop: $(document).height() }, 2000);
@@ -62,8 +72,23 @@ $(() => {
     $divs.show();
     $score.show();
     $play.hide();
+    $right.show();
+    $left.show();
     $stopTrack.hide();
   });
+  $play.on('click', () => {
+    generateAnimal();
+    fallOne();
+  });
+
+
+  $gameOver.on('mouseover', ()=>{
+    $gameOver.text(`Your score is: ${score}`);
+  });
+  $gameOver.on('mouseout', ()=>{
+    $gameOver.text('Game Over');
+  });
+
 
   $stopTrack.on('click', () => {
     $RedPlanet.pause();
@@ -75,24 +100,18 @@ $(() => {
       //keypress rigth moves right
     if (e.keyCode === 39) {
       if (x < 5) {
-        console.log('pressed right');
         x= x+1;
         $('.box'+ (42+x)).addClass('net');
         $('.box'+ (42+x-1)).removeClass('net');
-        console.log(x);
       }
     //keypress left moves left
     } else if(e.keyCode === 37) {
       if(x >0){
-        console.log('pressed left');
         x= x-1;
         $('.box'+ (42+x)).addClass('net');
         $('.box'+ (42+x+1)).removeClass('net');
-        console.log(x);
-
       }
     }
-
   });
 
 //generate the animal
@@ -101,19 +120,13 @@ $(() => {
     className = Math.random() > 0.5 ? 'cat' : 'dog';
     $(`.box${divNumber}`).addClass(className);
   }
-  console.log(divNumber);
 
-  $play.on('click', () => {
-    generateAnimal();
-    fallOne();
-  });
+
 
   // Make the animal fall
-  let falltime = 250;
 
   function fallOne() {
     if (livesNumber.length>0) {
-    // if (livesNumber>=0){
       if (falltime>= 100){
         falltime--;
       }
@@ -123,13 +136,11 @@ $(() => {
             $(`.box${divNumber}`).removeClass('cat');
             divNumber += 6;
             $(`.box${divNumber}`).addClass('cat');
-            // console.log('fallOne', divNumber);
             fallOne();
           } else {
             $(`.box${divNumber}`).removeClass('dog');
             divNumber += 6;
             $(`.box${divNumber}`).addClass('dog');
-            // console.log('fallOne', divNumber);
             fallOne();
           }
         } else {
@@ -141,6 +152,8 @@ $(() => {
     } else {
       ($(`.box${divNumber}`).removeClass('cat dog'));
       $gameOver.show();
+      $playAgain.show();
+      $article.hide();
       $section.hide();
       $divs.hide();
     }
@@ -149,27 +162,37 @@ $(() => {
   function checkCatch() {
     switch($(`.box${divNumber}`).hasClass('net')) {
       case true:
-        console.log('you caught an animal'+`.box${divNumber}`);
         score = score+1;
         $caught.play();
         $score.text(score);
         break;
       case false:
-        console.log('you lost a life');
         $byeLife.play();
         livesNumber.pop();
         $lives.html(livesNumber);
-        $lives.addClass('ringing');
-        console.log(livesNumber.length);
-
     } ($(`.box${divNumber}`).removeClass('cat dog'));
-    $lives.removeClass('ringing');
   }
 
-  function detectmob() {
-    if(('ontouchstart' in window)||(navigator.maxTouchPoints > 0)||(navigator.msMaxTouchPoints > 0))
-      document.body.className='touch';
-    else
-    document.body.className='no-touch';
-  }
+  // function detectmob() {
+  //   if(('ontouchstart' in window)||(navigator.maxTouchPoints > 0)||(navigator.msMaxTouchPoints > 0))
+  //     document.body.className='touch';
+  //   else
+  //   document.body.className='no-touch';
+  // }
+  $playAgain.on('click', () => {
+    console.log('click');
+    $section.show();
+    $divs.show();
+    $score.show();
+    $play.hide();
+    $right.show();
+    $left.show();
+    $stopTrack.hide();
+    $gameOver.hide();
+    $playAgain.hide();
+  });
+  $playAgain.on('click', () => {
+    generateAnimal();
+    fallOne();
+  });
 });
