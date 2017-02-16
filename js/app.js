8627d4c1a077299f623f29cb416e305085e13708
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2017-02-16 16:34:03 +0000

    final build of the game

--- js/app.js
+++ js/app.js
@@ -1,186 +1,197 @@
-$(() => {
-  console.log('all working boss');
-  let divNumber = null;
-  let className = null;
-  const $gameOver = $('.gameOver');
-  const $article = $('article');
-  const $caught = $('#caught').get(0);
-  const $byeLife = $('#lifeLost').get(0);
-  const $divs = $('div');
-  const $play = $('#play');
-  const $stopTrack = $('#stop');
-  const $RedPlanet = $('#soundtrack').get(0);
-  const $lives = $('.lives');
-  const $score = $('.score');
-  const $section = $('section');
-  const $playAgain = $('#playAgain');
-  const $left = $('#left');
-  const $right = $('#right');
-  const $pug = $('.pug');
-  const $portal = $('.portal');
-  let score = 0;
-  let g = 1;
-  let falltime = 250;
-  let livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
-  let x = 0;
-  console.log(livesNumber.length);
+var rain = rain || {};
 
-  $right.on('click', () => {
-    if (x < 5) {
-      x= x+1;
-      $('.box'+ (42+x)).addClass('net');
-      $('.box'+ (42+x-1)).removeClass('net');
+//------------------------- generate the animal --------------------------------
+rain.generateAnimal = function generateAnimal() {
+  this.divNumber = Math.floor(Math.random()*6);
+  this.className = Math.random() > 0.5 ? 'cat' : 'dog';
+  console.log(this.divNumber, this.className);
+  $(`.box${this.divNumber}`).addClass(this.className);
+};
+
+  //------------------------ Make the animal fall -------------------------------
+rain.fallOne = function fallOne() {
+  if (this.livesNumber.length>0) {
+    if (this.falltime>= 100){
+      this.falltime--;
     }
-  });
-
-  $left.on('click', ()=> {
-    if(x >0){
-      x= x-1;
-      $('.box'+ (42+x)).addClass('net');
-      $('.box'+ (42+x+1)).removeClass('net');
-    }
-  });
-
-  $lives.html(livesNumber);
-  $gameOver.hide();
-  $pug.hide();
-  $portal.hide();
-  $section.hide();
-  $divs.hide();
-  $score.hide();
-  $playAgain.hide();
-  $play.focus();
-  $article.show();
-  $right.hide();
-  $left.hide();
-
-  $play.on('click', ()=> {
-    $('html, body').animate({ scrollTop: $(document).height() }, 2000);
-    $section.show();
-    $divs.show();
-    $score.show();
-    $play.hide();
-    $right.show();
-    $left.show();
-    $stopTrack.hide();
-    $article.show();
-    $lives.html(livesNumber);
-    generateAnimal();
-    fallOne();
-  });
-
-
-  $gameOver.on('mouseover', ()=>{
-    $gameOver.text(`${score}`);
-  });
-  $gameOver.on('mouseout', ()=>{
-    $gameOver.text('Game Over');
-  });
-
-
-  $stopTrack.on('click', () => {
-    if ( g%2 !== 0){
-      $RedPlanet.pause();
-      $stopTrack.html('Play music');
-      g+=1;
-    } else {
-      $RedPlanet.play();
-      $stopTrack.html('Stop music');
-      g+=1;
-    }console.log(g);
-  });
-
-  //move net function
-  $('body').keydown(function(e) {
-
-      //keypress rigth moves right
-    if (e.keyCode === 39) {
-      if (x < 5) {
-        x= x+1;
-        $('.box'+ (42+x)).addClass('net');
-        $('.box'+ (42+x-1)).removeClass('net');
-      }
-    //keypress left moves left
-    } else if(e.keyCode === 37) {
-      if(x >0){
-        x= x-1;
-        $('.box'+ (42+x)).addClass('net');
-        $('.box'+ (42+x+1)).removeClass('net');
-      }
-    }
-  });
-
-//generate the animal
-  function generateAnimal() {
-    divNumber = Math.floor(Math.random()*6);
-    className = Math.random() > 0.5 ? 'cat' : 'dog';
-    console.log(divNumber, className);
-    $(`.box${divNumber}`).addClass(className);
-  }
-
-  // Make the animal fall
-  function fallOne() {
-    if (livesNumber.length>0) {
-      if (falltime>= 100){
-        falltime--;
-      }
-      setTimeout(() => {
-        if (divNumber <= 41) {
-          if (className==='cat'){
-            $(`.box${divNumber}`).removeClass('cat');
-            divNumber += 6;
-            $(`.box${divNumber}`).addClass('cat');
-            fallOne();
-          } else {
-            $(`.box${divNumber}`).removeClass('dog');
-            divNumber += 6;
-            $(`.box${divNumber}`).addClass('dog');
-            fallOne();
-          }
+    setTimeout(() => {
+      if (this.divNumber <= 41) {
+        if (this.className==='cat'){
+          $(`.box${this.divNumber}`).removeClass('cat');
+          this.divNumber += 6;
+          $(`.box${this.divNumber}`).addClass('cat');
+          this.fallOne();
         } else {
-          checkCatch();
-          generateAnimal();
-          fallOne();
+          $(`.box${this.divNumber}`).removeClass('dog');
+          this.divNumber += 6;
+          $(`.box${this.divNumber}`).addClass('dog');
+          this.fallOne();
         }
-      }, falltime);
-    } else {
-      ($(`.box${divNumber}`).removeClass('cat dog'));
-      $gameOver.show();
-      $playAgain.show();
-      $article.hide();
-      $section.hide();
-      $divs.hide();
-      $portal.show();
-      $pug.show();
-      $pug.animate({
-        marginLeft: '+=1700px'
-      }, 6000);
-    }
+      } else {
+        this.checkCatch();
+        this.generateAnimal();
+        this.fallOne();
+      }
+    }, this.falltime);
+//---------------------------- if you run out of lives ----------------------------
+  } else {
+    ($(`.box${this.divNumber}`).removeClass('cat dog'));
+    this.$gameOver.show();
+    this.$playAgain.show();
+    this.$article.hide();
+    this.$section.hide();
+    this.$divs.hide();
+    this.$portal.show();
+    this.$pug.show();
+    this.$pug.animate({
+      marginLeft: '+=1700px'
+    }, 6000);
   }
+};
+//------------------------ chech if you caught an animal---------------------
+rain.checkCatch = function checkCatch() {
+  switch($(`.box${this.divNumber}`).hasClass('net')) {
+    case true:
+      this.score = this.score+1;
+      this.$caught.play();
+      this.$score.text(this.score);
+      break;
+//----------------------- if not take a life away ------------------------------
+    case false:
+      this.$byeLife.play();
+      this.livesNumber.pop();
+      this.$lives.html(this.livesNumber);
+  } ($(`.box${this.divNumber}`).removeClass('cat dog'));
+};
 
-  function checkCatch() {
-    switch($(`.box${divNumber}`).hasClass('net')) {
-      case true:
-        score = score+1;
-        $caught.play();
-        $score.text(score);
-        break;
-      case false:
-        $byeLife.play();
-        livesNumber.pop();
-        $lives.html(livesNumber);
-    } ($(`.box${divNumber}`).removeClass('cat dog'));
-  }
+rain.setup = function() {
+  console.log('all working boss');
+  rain.$body = $('body');
+  rain.divNumber = null;
+  rain.className = null;
+  rain.$gameOver = $('.gameOver');
+  rain.$article = $('article');
+  rain.$caught = $('#caught').get(0);
+  rain.$byeLife = $('#lifeLost').get(0);
+  rain.$divs = $('div');
+  rain.$play = $('#play');
+  rain.$stopTrack = $('#stop');
+  rain.$RedPlanet = $('#soundtrack').get(0);
+  rain.$lives = $('.lives');
+  rain.$score = $('.score');
+  rain.$section = $('section');
+  rain.$playAgain = $('#playAgain');
+  rain.$left = $('#left');
+  rain.$right = $('#right');
+  rain.$pug = $('.pug');
+  rain.$portal = $('.portal');
+  rain.score = 0;
+  rain.g = 1;
+  rain.falltime = 250;
+  rain.livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
+  rain.x = 0;
+  console.log(rain.livesNumber.length);
+  rain.$lives.html(rain.livesNumber);
+  rain.$gameOver.hide();
+  rain.$pug.hide();
+  rain.$portal.hide();
+  rain.$section.hide();
+  rain.$divs.hide();
+  rain.$score.hide();
+  rain.$playAgain.hide();
+  rain.$play.focus();
+  rain.$article.show();
+  rain.$right.hide();
+  rain.$left.hide();
 
-  $playAgain.on('click', () => {
-    livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
-    falltime = 250;
-    score = 0;
-    $play.show();
-    $stopTrack.show();
-    $gameOver.hide();
-    $playAgain.hide();
-    $portal.hide();
-    $pug.stop().removeAttr('style').hide();
+  // ----------------------------- start the game ----------------------------
+  rain.$play.on('click', ()=> {
+    $('html, body').animate({ scrollTop: $(document).height() }, 2000);
+    this.$section.show();
+    this.$divs.show();
+    this.$score.show();
+    this.$play.hide();
+    this.$right.show();
+    this.$left.show();
+    this.$stopTrack.hide();
+    this.$article.show();
+    this.$lives.html(this.livesNumber);
+    this.generateAnimal();
+    this.fallOne();
   });
-});
+
+  // ------------------------ show the score -------------------------
+  rain.$gameOver.on('mouseover', ()=>{
+    this.$gameOver.text(`${this.score}`);
+  });
+  rain.$gameOver.on('mouseout', ()=>{
+    this.$gameOver.text('Game Over');
+  });
+
+  // ------------------------- Stop or Play music ------------------------
+  rain.$stopTrack.on('click', () => {
+    if ( this.g % 2 !== 0){
+      this.$RedPlanet.pause();
+      this.$stopTrack.html('Play music');
+      this.g += 1;
+    } else {
+      this.$RedPlanet.play();
+      this.$stopTrack.html('Stop music');
+      this.g += 1;
+    }console.log(this.g);
+  });
+
+  //--------------- move portal (net) function with arrow keys ------------------
+  rain.$body.keydown(function(e) {
+    console.log('inside body function');
+    //keypress rigth moves right
+    if (e.keyCode === 39) {
+
+      if (rain.x < 5) {
+        console.log('going in the if');
+
+        rain.x = rain.x +1;
+        $('.box'+ (42+rain.x)).addClass('net');
+        $('.box'+ (42+rain.x-1)).removeClass('net');
+      }
+      //keypress left moves left
+    } else if(e.keyCode === 37) {
+      console.log('going in the else if');
+      if(rain.x >0){
+        rain.x= rain.x-1;
+        $('.box'+ (42+rain.x)).addClass('net');
+        $('.box'+ (42+rain.x+1)).removeClass('net');
+      }
+    }
+  });
+  //------------------ move the portal (net) with arrow buttons-----------------------
+  rain.$right.on('click', () => {
+    if (rain.x < 5) {
+      rain.x = rain.x +1;
+      $('.box'+ (42+rain.x)).addClass('net');
+      $('.box'+ (42+rain.x-1)).removeClass('net');
+    }
+  });
+  rain.$left.on('click', ()=> {
+    if(rain.x >0){
+      rain.x= rain.x-1;
+      $('.box'+ (42+rain.x)).addClass('net');
+      $('.box'+ (42+rain.x+1)).removeClass('net');
+    }
+  });
+  //------------------------- click play again to show start ------------------------------
+  rain.$playAgain.on('click', () => {
+    this.livesNumber = ['&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;','&hearts;'];
+    this.falltime = 250;
+    this.score = 0;
+    this.$play.show();
+    this.$stopTrack.show();
+    this.$gameOver.hide();
+    this.$playAgain.hide();
+    this.$portal.hide();
+    this.$pug.stop().removeAttr('style').hide();
+  });
+};
+
+
+$(rain.setup.bind(rain));
